3edd443aa0d9f8bf810769f6c197f06ebbd2bfec
seynurmammadov <seynursm@code.edu.az>
2022-11-02 16:07:43 +0400

    Updated apply.sh

--- apply.sh
+++ apply.sh
@@ -1,5 +1,5 @@
-#python ./script/main.py test-edit
-#python ./script/main.py test-pr PB-Digital <token>
-#python ./script/main.py test-edit-and-pr PB-Digital <token>
-#python ./script/main.py test-create PB-Digital <token>
-#python ./script/main.py test-edit-pr-create PB-Digital <token>
+#python ./script/main.py test/test-edit
+#python ./script/main.py test/test-pr PB-Digital <token>
+#python ./script/main.py test/test-edit-and-pr PB-Digital <token>
+#python ./script/main.py test/test-create PB-Digital <token>
+#python ./script/main.py test/test-edit-pr-create PB-Digital <token>
