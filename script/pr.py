44346ae0d3595e0616a59a63328f73f911f16cd3
seynurmammadov <seynursm@code.edu.az>
2022-12-10 02:33:32 +0400

    Changed pr action execution through rest api

--- script/pr.py
+++ script/pr.py
@@ -1,8 +1,7 @@
 import os
-import shutil
 import time
 
-from helper import get_ms_list, cancel_pipeline, validate_ms
+from helper import get_ms_list
 
 
 def open_pr(data):
@@ -10,16 +9,12 @@ def open_pr(data):
     ms_list = get_ms_list(data.get('repos'))
     for ms in ms_list:
         ms = ms.replace('\n', '')
-        url = "https://oauth2:{}@github.com/{}/{}.git".format(os.getenv('GITHUB_TOKEN'), org, ms)
-        os.system('git clone {}'.format(url))
-        if not validate_ms(ms):
-            continue
-        os.system('git remote set-url origin {}'.format(url))
-        os.chdir(ms)
-        os.system("git checkout {}".format(data.get('from')))
-        os.system('gh pr create -B {} -a {} -t "{}" -b "{}" '.format(data.get('to'), data.get('assignee'),
-                                                                     data.get('name'), data.get('body')))
-        cancel_pipeline(data)
-        os.chdir('../')
-        shutil.rmtree(ms)
-        time.sleep(5)
+        print('Creating pull request for {} into {} in {}/{}'.format(data.get('from'), data.get('to'), org, ms))
+        os.system('gh api --method POST -H "Accept: application/vnd.github+json" /repos/{}/{}/pulls -f title=\'{}\' '
+                  '-f body=\'{}\' -f head=\'PB-Digital:{}\' -f base=\'{}\' '.format(org,
+                                                                                    ms,
+                                                                                    data.get('name'),
+                                                                                    data.get('body'),
+                                                                                    data.get('from'),
+                                                                                    data.get('to')))
+        time.sleep(1.5)
